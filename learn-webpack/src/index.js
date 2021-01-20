80270a30365daa06f0a4d7475589479e7b691477
Vadim <34789596+vadimoarturo@users.noreply.github.com>
2021-01-20 16:14:59 +0500

    Add files via upload

--- learn-webpack/src/index.js
+++ learn-webpack/src/index.js
@@ -1,1 +1,16 @@
+import {config} from './modules/config'
+import AppService from './modules/app.service'
+import './css/index.css'
+import './less/index.less'
+import './scss/index.scss'
+import React from 'react'
+import {render} from 'react-dom'
+import App from './App'
+import './modules/ts.module.ts'
 
+console.log(config.key);
+
+const service = new AppService('Hello world!')
+service.log()
+
+render(<App />, document.getElementById('app'))
